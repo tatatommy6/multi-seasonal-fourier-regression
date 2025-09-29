2445927977eb58838ec45a48946dbe0761e56da7
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-09-29 13:36:08 +0900

    Update core.py

--- msfr/core.py
+++ msfr/core.py
@@ -8,10 +8,14 @@ from torch.nn import functional as F # 뭔까 쓸거 같아서 일단 임포트
 import numpy as np
 
 class MSFR(nn.Module):
-    def __init__ (self, seasonal_periods, n_harmonics=3, trend="linear", reg_lambda=0.0, device = None):
+    def __init__ (self,input_dim, output_dim, n_harmonics=3, trend="linear", reg_lambda=0.0,device = None):
+        #n_harmonics:주기별 세밀함 정도
+        #trend: 계절성 외에 전체 추세 반영 방식
+        #reg_lambda: 정규화 강도
         
         super().__init__()
-        self.seasonal_periods = seasonal_periods
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         self.n_harmonics = n_harmonics
         self.trend = trend
         self.reg_lambda = reg_lambda
@@ -25,3 +29,16 @@ class MSFR(nn.Module):
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         return torch.zeros(1,1) # 임시값
         # return F.linear(input, self.weight, self.bias) 선형회귀 코드에서 가져옴
+
+
+
+
+    # def forward(self, t: torch.Tensor) -> torch.Tensor:
+    #     t = t.view(-1, 1)  # (B,1)
+    #     feats = []
+    #     for P in self.seasonal_periods:
+    #         for k in range(1, self.n_harmonics + 1):
+    #             w = 2.0 * math.pi * k / P
+    #             feats.append(torch.sin(w * t))
+    #             feats.append(torch.cos(w * t))
+    #     return torch.cat(feats, dim=1)  # (B, num_features)
