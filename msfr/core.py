b0c25417d706c26cfdb13d071347e20789990be2
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-11-16 00:53:52 +0900

    Changes to be committed: revert and reduce gain 	modified:   benchmark/shape_test/shape_test.ipynb 	modified:   msfr/core.py

--- msfr/core.py
+++ msfr/core.py
@@ -14,7 +14,7 @@ class MSFR(nn.Module):
 
     # TODO: 주석이 너무 많아 복잡해보임 -> 정리 필요
     
-    def __init__(self, input_dim, output_dim, n_harmonics=3, grad_scale=1.0, trend=None, device=None):
+    def __init__(self, input_dim, output_dim, n_harmonics=3, trend=None, device=None):
         super().__init__()
         self.n_harmonics = n_harmonics
         self.trend = trend # 추세는 잠시 사용하지 않기로, 더 알아보고 싶음
@@ -27,30 +27,19 @@ class MSFR(nn.Module):
         self.cycle = Parameter(torch.empty(input_dim, device=device))
         self.reset_parameters()
         
-        # 파라미터들에 대한 그래디언트 후크 설정 -> 그레디언트 스케일링 -> 학습 반영 증가
-        g = lambda g: torch.clamp(g * grad_scale, -1e6, 1e6)
-        self.weight.register_hook(g)
-        self.bias.register_hook(g)
-        self.cycle.register_hook(g)
-
     def reset_parameters(self) -> None:
-        # 선형회귀 초기화 방식에서 참고
-        nn.init.xavier_uniform_(self.weight, gain=2.0)
+        nn.init.xavier_uniform_(self.weight, gain=0.2)  # gain 값 감소
         fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
         bound = 1.0 / math.sqrt(max(1, fan_in))
         nn.init.uniform_(self.bias, -bound, bound)
-        nn.init.uniform_(self.cycle, 1.0, 10.0)  # 주기 초기값을 1~10 사이로 설정
+        nn.init.uniform_(self.cycle, 5.0, 8.0)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x_mean = x.mean(dim=0, keepdim=True)
-        x_std = x.std(dim=0, keepdim=True) + 1e-6
-        x_norm = (x - x_mean) / x_std # 정규화
-
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
         harmonics = torch.arange(1, self.n_harmonics + 1, device=input.device).float()  # (n_harmonics,)
-        cycles = F.softplus(self.cycle) # 주기 양수화
+        cycles = F.softplus(self.cycle) + 1e-3 # 주기 양수화
 
         # 브로드캐스팅을 위해 차원 정렬
-        x = x_norm.unsqueeze(-1)                # (batch_size, input_dim, 1)
+        x = input.unsqueeze(-1)                # (batch_size, input_dim, 1)
         harmonics = harmonics.view(1, 1, -1)    # (1, 1, n_harmonics)
         cycle = cycles.view(1, -1, 1)           # (1, input_dim, 1)
 
